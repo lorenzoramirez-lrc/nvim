f3f18b445e00d30cc65e2cceafde79e33ff593d3
Lorenzo Ramirez <lorenzoramirez.lrc@gmail.com>
2026-01-19 19:21:43 -0500

    Added auto-pair

--- lua/lorenzo/lazy.lua
+++ lua/lorenzo/lazy.lua
@@ -42,7 +42,14 @@ local plugins = {
             {'hrsh7th/cmp-nvim-lsp'}, -- Required
             {'L3MON4D3/LuaSnip'},     -- Required
         }
-    }   
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    }
 }
 
 local opts = {}
